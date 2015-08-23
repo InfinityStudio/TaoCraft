7162f4b568190ce84aee51eecc55c8fd75c24538
xfeng <xfeng@xfeng-PC>
2015-08-24 00:31:04 +0800

    map showed

--- src/main/java/astronomy/taocraft/client/gui/GuiResearch.scala
+++ src/main/java/astronomy/taocraft/client/gui/GuiResearch.scala
@@ -22,13 +22,15 @@ import org.lwjgl.opengl.GL11;
 class GuiResearch extends GuiScreen {
 	def portstartX = (width - frametextureX) / 2
 	def portstartY = (height - frametextureY) / 2
+	def mapstartX = (width - mapportX) / 2
+	def mapstartY = (height - mapportY) / 2
 	val frametextureX: Int = 256
 	val frametextureY: Int = 230
 	val mapportX: Int = 224
 	val mapportY: Int = 196
-	val framewidthX: Int = 8
-	val framewidthY: Int = 8
+  val maptexturesize = 512
 	val frameResourceLocation = new ResourceLocation("textures/gui/gui_research.png");
+	val mapResourceLocation = new ResourceLocation("textures/gui/gui_researchback.png");
 	var scale: Double = 1.0
 	var mouseXprevent: Int = 0
 	var mouseYprevent: Int = 0
@@ -40,25 +42,28 @@ class GuiResearch extends GuiScreen {
 	override def drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float) = {
 		transviewport(mouseX, mouseY);
 		super.drawScreen(mouseX, mouseY, partialTicks);
-		this.drawDefaultBackground();
-		drawframe();
+		drawDefaultBackground();
 		drawResearch();
+		drawframe();
 	}
 	def transviewport(mouseX: Int, mouseY: Int) = {
 		if (Mouse.isButtonDown(0)) {
-			val mapstartX = portstartX + framewidthX;
-			val mapstartY = portstartY + framewidthY;
 
-			if (mouseX >= mapstartX && mouseX < mapstartX + mapportX && mouseY >= mapstartY && mouseY < mapstartY + mapportY) {
+
+			if (mouseX >= mapstartX && mouseX < mapstartX + mapportX && mouseY >= mapstartY && mouseY < mapstartY + mapportY&&mouseXprevent>0&&mouseYprevent>0) {
 				mapcutX -= ((mouseX - mouseXprevent) * scale);
-				mapcutX = if (mapcutX < 0.0) 0.0 else if (mapcutX > 300.0) 300.0 else mapcutX
+				mapcutX = if (mapcutX < 0.0) 0.0 else if (mapcutX > maptexturesize-mapportX) maptexturesize-mapportX else mapcutX
 				mapcutY -= ((mouseY - mouseYprevent) * scale);
-				mapcutY = if (mapcutY < 0.0) 0.0 else if (mapcutY > 300.0) 300.0 else mapcutY
+				mapcutY = if (mapcutY < 0.0) 0.0 else if (mapcutY > maptexturesize-mapportY) maptexturesize-mapportY else mapcutY
 			}
 			this.mouseXprevent = mouseX;
 			this.mouseYprevent = mouseY;
 		}
-
+		else
+		{
+			this.mouseXprevent = -1;
+			this.mouseYprevent = -1;
+		}
 	}
 	def drawframe() = {
 		GlStateManager.enableBlend()
@@ -67,7 +72,7 @@ class GuiResearch extends GuiScreen {
 		drawTexturedModalRect(portstartX, portstartY, 0, 0, frametextureX, frametextureY)
 	}
 	def drawResearch() = {
-		
-
+		this.mc.getTextureManager().bindTexture(mapResourceLocation);
+		drawTexturedModalRect(mapstartX, mapstartY, mapcutX.toInt, mapcutY.toInt, mapportX, mapportY)
 	}
 }
