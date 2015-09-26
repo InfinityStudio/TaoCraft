a5c0a41b1ced02373941b7e0acc694705436bb4e
Blealtan Cao <bolee159632@live.com>
2015-09-26 13:50:17 +0800

    Ore add trying. Block registry added.

--- src/main/scala/astronomy/taocraft/ModBlocks.scala
+++ src/main/scala/astronomy/taocraft/ModBlocks.scala
@@ -1,20 +1,23 @@
 package astronomy.taocraft
+
+import astronomy.taocraft.block.LeadOre
 import net.minecraft.block.Block
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.fml.common.registry.GameRegistry
+import net.minecraft.creativetab.CreativeTabs
+
 /**
- * @author XFeng
+ * @author Blealtan
  */
 class ModBlocks {
   val Blocks = blocklist.toMap
   def blockinit[T <: Block](block: T, name: String) = {
-    GameRegistry.registerBlock(block, name);
-    block.setUnlocalizedName(name);
-    block.setCreativeTab(CreativeTabs.tabMisc);
+    GameRegistry.registerBlock(block, name)
+    block.setUnlocalizedName(name)
+    block.setCreativeTab(CreativeTabs.tabMisc)
     name -> block
   }
   def blocklist = {
-
+    blockinit(new LeadOre, "leadore")
     Nil
   }
 
